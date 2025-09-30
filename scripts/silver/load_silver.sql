d4a835e7e2fad964c0a6a266424f32bfbc541fea
alexebe <alexauguste01@gmail.com>
2025-09-30 19:46:40 +0000

    Cntry column silver.erp_loc_a101

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -159,10 +159,12 @@ INSERT INTO silver.erp_loc_a101
     cntry
 )
 select 
-trim(cid),
+TRIM(REPLACE(cid, '-', '')) cid,
 CASE 
-    WHEN len(cntry) >= 1 THEN TRIM(UPPER(cntry))
-    WHEN (len(cntry) = 0 OR cntry is null) THEN 'n/a'
+    WHEN TRIM(cntry) = 'DE'  THEN 'Germany'
+    WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+    WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+    ELSE TRIM(cntry)
 END cntry
 from bronze.erp_loc_a101;
 
